59fe44115301a8f818c7fbb748a05c05f7dc0980
Adrian Rami Piramide <230053@fppiramide.com>
2026-02-09 13:20:17 +0100

    dependencias

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('hola asdasd')
+})
+
+app.listen(8080, () => {
+    console.log('corriendo');
+    
+})
